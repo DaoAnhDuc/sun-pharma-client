a90d5b467f8d82a7933a08eb6bed10d04970da76
dinhdt@aladintech.co <dinhdt@aladintech.co>
2025-06-09 22:51:41 +0700

    oke

--- src/pages/GioiThieu.jsx
+++ src/pages/GioiThieu.jsx
@@ -7,14 +7,14 @@ const GioiThieu = () => {
     <div className="lg:py-30 py-10">
       <div className="container mx-auto grid lg:grid-cols-2 grid-cols-1 gap-10 p-4">
         <div className="relative">
-          <div className="absolute top-10 bg-[var(--primary-color)] text-white w-48 h-48 flex justify-center items-center flex-col rounded-2xl gap-2 md:scale-100 scale-75">
+          <div className="absolute lg:scale-100 md:scale-[0.8] scale-[0.6] top-10 bg-[var(--primary-color)] text-white w-48 h-48 flex justify-center items-center flex-col rounded-2xl gap-2">
             <div className="text-5xl font-bold">25+</div>
             <div>Year of experience</div>
           </div>
           <div className="pl-16 pr-16">
             <img src="https://html.themewant.com/invena/assets/images/about/04.webp" className="" alt="" />
           </div>
-          <div className="absolute bottom-10 right-0  md:scale-100 scale-75">
+          <div className="absolute bottom-10 right-0 lg:scale-100 md:scale-[0.8] scale-[0.6]">
             <img src="https://html.themewant.com/invena/assets/images/about/05.webp" className="w-40" alt="" />
           </div>
         </div>
